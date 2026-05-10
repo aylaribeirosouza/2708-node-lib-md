9d46079d69e9ae5ab8cc937e17e7ed53a0e083f4
aylaribeirosouza <ayla.ribeiro.souza@escola.pr.gov.br>
2026-05-10 22:16:02 +0000

    Trabalho 1 Finalizado

---
+++ index.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+const texto = "A sequência do sucesso de suspense A Empregada 2026 foi confirmada, intitulada O Segredo da Empregada, com estreia nos cinemas prevista para 17 de dezembro de 2027. O novo filme, baseado no segundo livro de Freida McFadden, trará o retorno de Sydney Sweeney como Millie e Paul Feig na direção";
+
+console.log(chalk.magenta(texto)); 
